b2d7aebe548e1eb064fbac1c8a2f8dd820bc260c
Lluís Gili <lluis@ingent.net>
2017-12-05 21:38:07 +0100

    external nodes

---
+++ puppetmaster/conf/external_node.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# TODO: host IP is always 172.18.0.1 ?
+URL="http://172.18.0.1:3000/node/get_host_definition?hostname=$1"
+
+if [ -d /usr/local/cache/ ]; then
+
+  echo "cached $1" >> /usr/local/cache/log
+  /usr/bin/wget -O "/usr/local/cache/$1.yaml" -q $URL
+  /bin/cat "/usr/local/cache/$1.yaml"
+
+else
+  
+  /usr/bin/wget -q $URL
+
+fi
