cd797dd5b8db4e3372a17fe8460c1e130db945fd
David Escala <descala@ingent.net>
2017-12-07 13:46:33 +0100

    Fix hostnames in the case where host runs with a domain name

--- puppetmaster/conf/external_node.sh
+++ puppetmaster/conf/external_node.sh
@@ -3,6 +3,13 @@
 # TODO: host IP is always 172.18.0.1 ?
 URL="http://172.18.0.1:3000/node/get_host_definition?hostname=$1"
 
+if [ "$1" = "puppetdb.foo.local" ]; then
+  echo '---
+parameters:
+classes:'
+  exit
+fi
+
 if [ -d /usr/local/cache/ ]; then
 
   echo "cached $1" >> /usr/local/cache/log
